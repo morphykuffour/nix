5f752df86d94da94d82f7b2b139e5d34e1232fa3
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-06-22 20:26:20 +0000

    adding WSL-NixOs

---
+++ configuration.nix
@@ -0,0 +1,46 @@
+{ lib, pkgs, config, modulesPath, ... }:
+
+with lib;
+let
+  nixos-wsl = import ./nixos-wsl;
+in
+{
+  imports = [
+    "${modulesPath}/profiles/minimal.nix"
+
+    nixos-wsl.nixosModules.wsl
+  ];
+
+  wsl = {
+    enable = true;
+    automountPath = "/mnt";
+    defaultUser = "nixos";
+    startMenuLaunchers = true;
+
+    # Enable integration with Docker Desktop (needs to be installed)
+    # docker.enable = true;
+  };
+
+  # Enable nix flakes
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  environment.systemPackages = with pkgs;  [
+      wget 
+      vim
+      zsh
+      neovim
+      git
+      curl
+      jq
+      emacs
+      file
+      exa
+      bat
+      rsync
+      stow
+      binutils
+  ];
+}
