be7cf5f3dbdab6561eb23116cacf03e029844545
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-07-08 13:00:00 -0400

    I think flake was fixed; added macos

--- flake.nix
+++ flake.nix
@@ -1,91 +1,3 @@
-#{
-#  description = "an ambitous flake for linux, macos, win11-wsl";
-
-#  inputs = {
-#    # utils.url = "github:morphykuffour/nix";
-
-#    nixpkgs = {
-#      url = "github:nixos/nixpkgs/nixos-22.05";
-#      inputs.nixpkgs.follows = "nixpkgs";
-#    };
-
-#    home-manager = {
-#      url = "github:nix-community/home-manager/release-22.05";
-#      inputs.nixpkgs.follows = "nixpkgs";
-#    };
-
-#    # NUR packages
-#    nur = {
-#      url = "github:nix-community/NUR";
-#      inputs.nixpkgs.follows = "nixpkgs";
-#    };
-
-#    # macOS 
-#    darwin = {
-#      url = "github:lnl7/nix-darwin/master";
-#      inputs.nixpkgs.follows = "nixpkgs";
-#    };
-
-#    # windows wsl
-#    wsl = {
-#      url = "github:nix-community/NixOS-WSL";
-#      inputs.nixpkgs.follows = "nixpkgs";
-#    };
-
-#    neovim.url = "github:neovim/neovim?dir=contrib";
-#  };
-
-#  outputs =
-#    inputs@{ self
-#      # , utils
-#    , nixpkgs
-#    , home-manager
-#    , nur
-#    , darwin
-#    , wsl
-#    , neovim
-#    , ...
-#    }:
-#    let
-#      #variables
-#      user = "morp";
-#      pkgs = inputs.unstable.legacyPackages.x86_64-linux;
-#    in
-#    {
-#      # NixOS configurations
-#      nixosConfigurations = (
-#        import ./nixos {
-#          inherit (nixpkgs) lib;
-#          inherit inputs nixpkgs home-manager nur user;
-#        }
-#      );
-
-#      # macos configurations
-#      # darwinConfigurations = (
-#      #   import ./darwin {
-#      #     inherit (nixpkgs) lib;
-#      #     inherit inputs nixpkgs home-manager darwin user;
-#      #   }
-#      # );
-
-#      # wsl configurations
-#      # wslConfigurations = (
-#      #   import ./wsl {
-#      #     inherit (nixpkgs) lib;
-#      #     inherit inputs nixpkgs home-manager nur user;
-#      #   }
-#      # );
-
-#      # os-agnotisc configurations
-#      # homeConfigurations = (
-#      #   import ./home.nix {
-#      #     inherit (nixpkgs) lib;
-#      #     inherit inputs nixpkgs home-manager user;
-#      #   }
-#      # );
-#    };
-#}
-
 {
   inputs = {
     nixpkgs = {
@@ -141,6 +53,7 @@
       lib = nixpkgs.lib;
     in
     {
+      # NixOS configurations
       nixosConfigurations = {
         morp = lib.nixosSystem {
           inherit system;
@@ -151,6 +64,14 @@
           ];
         };
       };
+
+      # macos configurations
+      darwinConfigurations = (
+        import ./darwin {
+          inherit (nixpkgs) lib;
+          inherit inputs nixpkgs home-manager darwin user;
+        }
+      );
 
       # wsl configurations
       wslConfigurations = (
