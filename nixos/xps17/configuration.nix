8824fab977c791df5701c174d5e0c8f8a13f20ee
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-07-08 09:52:04 -0400

    fixing circular bug

--- nixos/xps17/configuration.nix
+++ nixos/xps17/configuration.nix
@@ -357,6 +357,12 @@
   environment.pathsToLink = [ "/libexec" ];
   system.stateVersion = "22.05";
 
+  #temporary bluetooth fix
+  systemd.tmpfiles.rules = [
+    "d /var/lib/bluetooth 700 root root - -"
+  ];
+  systemd.targets."bluetooth".after = [ "systemd-tmpfiles-setup.service" ];
+
   # use flakes
   nix = {
     package = pkgs.nixFlakes;
