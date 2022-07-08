cb1b81c11bfb71229236963e403d26149495b66d
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-07-08 13:57:12 -0400

    flakes are hard to work with will get back to it later when nixos knowledge is stronger or when flakes are more mature

--- nixos/xps17/picom.nix
+++ nixos/xps17/picom.nix
@@ -3,18 +3,6 @@
 {
   services.picom = {
     enable = true;
-    package = pkgs.picom.overrideAttrs (o: {
-      src = pkgs.fetchFromGitHub {
-        #repo = "picom";
-        #owner = "pijulius";
-        #rev = "982bb43e5d4116f1a37a0bde01c9bda0b88705b9";
-        #sha256 = "YiuLScDV9UfgI1MiYRtjgRkJ0VuA1TExATA2nJSJMhM=";
-        repo = "picom";
-        owner = "jonaburg";
-        rev = "e3c19cd7d1108d114552267f302548c113278d45";
-        sha256 = "0000000000000000000000000000000000000000000000000000";
-      };
-    });
     backend = "glx";
     vSync = true;
     inactiveOpacity = 0.93;
@@ -22,7 +10,7 @@
     menuOpacity = 0.93;
 
     opacityRules = [
-      "100:name = 'brave-browser'"
+      "100:name = 'brave'"
       "100:name = 'Picture in picture'"
       "100:name = 'Picture-in-Picture'"
       "100:class_g = 'rofi'"
