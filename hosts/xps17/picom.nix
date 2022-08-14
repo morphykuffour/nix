618b38c3cc741f713a3d73509db5bede16f73548
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-08-14 07:36:58 -0400

    few changes to picom.nix: renamed new one to picom.nix and testing it out on my DE

--- hosts/xps17/picom.nix
+++ hosts/xps17/picom.nix
@@ -1,40 +1,79 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-{
-  services.picom = {
-    enable = true;
-    inactiveOpacity = 0.8;
-    activeOpacity = 1.0;
-    opacityRules = [
-      "100:class_g *= 'brave-browser'"
-      "100:class_g = 'rofi'"
-      "70:class_g = 'kitty'"
-      "70:class_g = 'nvim'"
-      "70:class_g = 'vim'"
-    ];
-    fade = true;
-    shadow = true;
-    fadeDelta = 4;
-    fadeSteps = [ 0.02 0.02 ];
-    settings = {
-      blur = true;
-      frame-opacity = 1;
-      blur-background = true;
-      inactive-opacity-override = false;
-      blur-kern = "7x7box";
-      blur-background-exclude = [ "class_g = 'brave-browser'" ];
-      inactiveDim = "0.2";
-      focus-exclude = [
-        "class_g = 'brave-browser'"
-        "class_g = 'firefox'"
-        "class_g = 'Google-chrome'"
-      ];
+{ 
+  config = lib.mkIf (config.xsession.enable) {      # Only evaluate code if using X11
+    services.picom = {
+      enable = true;
+      package = pkgs.picom.overrideAttrs(o: {
+        src = pkgs.fetchFromGitHub {
+          #repo = "picom";
+          #owner = "pijulius";
+          #rev = "982bb43e5d4116f1a37a0bde01c9bda0b88705b9";
+          #sha256 = "YiuLScDV9UfgI1MiYRtjgRkJ0VuA1TExATA2nJSJMhM=";
+          repo = "picom";
+          owner = "jonaburg";
+          rev = "e3c19cd7d1108d114552267f302548c113278d45";
+          sha256 = "4voCAYd0fzJHQjJo4x3RoWz5l3JJbRvgIXn1Kg6nz6Y=";
+        };
+      });                                           # Override picom to use pijulius' version
+
+      backend = "glx";                              # Rendering either with glx or xrender. You'll know if you need to switch this.
+      vSync = true;                                 # Should fix screen tearing
+
+      activeOpacity = "0.93";                       # Node transparency
+      inactiveOpacity = "0.93";
+      menuOpacity = "0.93";
+
+      shadow = false;                               # Shadows
+      shadowOpacity = "0.75";
+      fade = true;                                  # Fade
+      fadeDelta = 10;
+      opacityRule = [                               # Opacity rules if transparency is prefered
+        "100:name = 'Picture in picture'"
+        "100:name = 'Picture-in-Picture'"
+        "85:class_i ?= 'rofi'"
+        "80:class_i *= 'discord'"
+        "80:class_i *= 'emacs'"
+        "80:class_i *= 'Kitty'"
+        "100:class_i *= 'Brave-Browser'"
+        "100:fullscreen"
+      ];                                            # Find with $ xprop | grep "WM_CLASS"
+
+      extraOptions = ''
+        daemon = true;
+        use-damage = false;                         # Fixes flickering and visual bugs with borders
+        resize-damage = 1
+        refresh-rate = 0;
+        corner-radius = 5;                          # Corners
+        round-borders = 5;
+
+        # Animations Pijulius
+        #animations = true;                          # All Animations
+        #animation-window-mass = 0.5;
+        #animation-for-open-window = "zoom";
+        #animation-stiffness = 350;
+        #animation-clamping = false;
+        #fade-out-step = 1;                          # Will fix random border dots from not disappearing
+
+        # Animations Jonaburg
+        transition-length = 300
+        transition-pow-x = 0.5
+        transition-pow-y = 0.5
+        transition-pow-w = 0.5
+        transition-pow-h = 0.5
+        size-transition = true
+
+        # Extras
+        detect-rounded-corners = true;              # Below should fix multiple issues
+        detect-client-opacity = false;
+        detect-transient = true
+        detect-client-leader = false
+        mark-wmwim-focused = true;
+        mark-ovredir-focues = true;
+        unredir-if-possible = true;
+        glx-no-stencil = true;
+        glx-no-rebind-pixmap = true;
+      '';                                           # Extra options for picom.conf (mostly for pijulius fork)
     };
-    backend = "glx";
-    vSync = true;
-
-    # extraOptions = ''
-    # '';
-
   };
 }
