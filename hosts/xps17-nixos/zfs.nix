6da59d8530ec2dbb18b26504e11730d75ccc501a
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-11-05 17:00:44 -0400

    trying to get zfs to work on nixos

--- hosts/xps17-nixos/zfs.nix
+++ hosts/xps17-nixos/zfs.nix
@@ -1,8 +1,11 @@
+# https://nixos.wiki/wiki/ZFS
 {
   config,
   pkgs,
   ...
 }: {
+  # /dev/nvme0n1 -> primary disk for ext4 fs
+  # /dev/nvme1n1 -> secondary disk for zfs fs
   boot.supportedFilesystems = ["zfs"];
   networking.hostId = "319155cd";
 }
