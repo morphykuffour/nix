cb930de8424e6e507936268fe6f8f3a99c91feef
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-11-06 21:53:35 -0500

    added i3 to optiplex-nixos

--- hosts/optiplex-nixos/configuration.nix
+++ hosts/optiplex-nixos/configuration.nix
@@ -57,6 +57,18 @@
           };
           sddm.enable = true;
       };
+
+      windowManager.i3 = {
+        enable = true;
+        package = pkgs.i3-gaps;
+
+        extraPackages = with pkgs; [
+          dmenu
+          i3status
+          i3lock
+          sxhkd
+        ];
+      };
     };
   };
 
