f8db181675b1cd5a50532775a967925458e3b904
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-11-07 17:41:34 -0500

    added tailscale to optiplex-nixos

--- hosts/optiplex-nixos/configuration.nix
+++ hosts/optiplex-nixos/configuration.nix
@@ -169,6 +169,57 @@
     bat
     which
     gnumake
+    tailscale
   ];
   nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # tailscale
+  # enable the tailscale service
+  services.tailscale.enable = true;
+
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = ["network-pre.target" "tailscale.service"];
+    wants = ["network-pre.target" "tailscale.service"];
+    wantedBy = ["multi-user.target"];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey tskey-auth-kgVdsP4CNTRL-bbiZywNy4EdRgztF3x3DHdbxTQ5y3d8w
+    '';
+  };
+
+  networking.firewall = {
+    
+    # warning: Strict reverse path filtering breaks Tailscale 
+    # exit node use and some subnet routing setups.
+  checkReversePath = "loose";
+    # enable the firewall
+    enable = true;
+
+    # always allow traffic from your Tailscale network
+    trustedInterfaces = ["tailscale0"];
+
+    # allow the Tailscale UDP port through the firewall
+    allowedUDPPorts = [config.services.tailscale.port];
+
+    # allow you to SSH in over the public internet
+    allowedTCPPorts = [22];
+  };
 }
