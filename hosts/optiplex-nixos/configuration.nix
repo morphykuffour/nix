fc73c3ac1012fe39b6d46bb95e7084eb89a21ae8
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-11-07 21:38:49 -0500

    TODO fix the tailscale

--- hosts/optiplex-nixos/configuration.nix
+++ hosts/optiplex-nixos/configuration.nix
@@ -201,7 +201,7 @@
       fi
 
       # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up -authkey tskey-auth-kgVdsP4CNTRL-bbiZywNy4EdRgztF3x3DHdbxTQ5y3d8w
+      ${tailscale}/bin/tailscale up --authkey=${cfg.autoprovision.key}
     '';
   };
 
@@ -223,3 +223,5 @@
     allowedTCPPorts = [22];
   };
 }
+# private key for optiplex-nixos
+# tskey-auth-kgVdsP4CNTRL-bbiZywNy4EdRgztF3x3DHdbxTQ5y3d8w
