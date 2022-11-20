6854badc2420cddcae0cdc56d269197f494a171c
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-11-20 17:23:57 -0500

    adding kernel module

--- hosts/xps17-nixos/zfs.nix
+++ hosts/xps17-nixos/zfs.nix
@@ -4,8 +4,9 @@
   pkgs,
   ...
 }: {
-  # /dev/nvme0n1 -> primary disk for ext4 fs
-  # /dev/nvme1n1 -> secondary disk for zfs fs
+  # /dev/nvme0n1 -> primary disk for ext4 fs => NixOs
+  # /dev/nvme1n1 -> secondary disk for zfs fs => RedNixOS and Arch Linux
   boot.supportedFilesystems = ["zfs"];
   networking.hostId = "319155cd";
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 }
