dace6061837748f78d384e3102ff4b86482ea1e5
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-12-10 13:09:42 -0500

    few changes

--- hosts/rpi3b-nixos/rpi.nix
+++ hosts/rpi3b-nixos/rpi.nix
@@ -1,5 +1,9 @@
-{ config, pkgs, lib, ... }:
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   # NixOS wants to enable GRUB by default
   boot.loader.grub.enable = false;
 
@@ -43,6 +47,10 @@
 
   # Use 1GB of additional swap memory in order to not run out of memory
   # when installing lots of things while running other things at the same time.
-  swapDevices = [ { device = "/swapfile"; size = 1024; } ];
-
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 1024;
+    }
+  ];
 }
