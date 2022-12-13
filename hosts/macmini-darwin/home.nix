9e80a666fe59014c250eb85291c0b832d1e91768
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-12-13 13:17:27 -0500

    .

--- hosts/macmini-darwin/home.nix
+++ hosts/macmini-darwin/home.nix
@@ -29,89 +29,89 @@
 
       # utilities packages
       p7zip
-      eva
-      # mail packages
-      # himalaya
-      # neomutt
-      # aria2
-      # isync
-      # msmtp
-      # pass
-      # mu
-      ripgrep
-      autojump
-      pandoc
-      croc
-      mpv
-      feh
+      # eva
+      # # mail packages
+      # # himalaya
+      # # neomutt
+      # # aria2
+      # # isync
+      # # msmtp
+      # # pass
+      # # mu
+      # ripgrep
+      # autojump
+      # pandoc
+      # croc
+      # mpv
+      # feh
 
-      # edit packages
-      tree-sitter-grammars.tree-sitter-markdown
-      nodePackages.typescript-language-server
-      nodePackages.bash-language-server
-      sumneko-lua-language-server
-      nodePackages.typescript
-      nodePackages.prettier
-      nodePackages.pyright
-      nodePackages.insect
-      rust-analyzer
-      rnix-lsp
-      ccls
-      black
-      gopls
-      ccls
+      # # edit packages
+      # tree-sitter-grammars.tree-sitter-markdown
+      # nodePackages.typescript-language-server
+      # nodePackages.bash-language-server
+      # sumneko-lua-language-server
+      # nodePackages.typescript
+      # nodePackages.prettier
+      # nodePackages.pyright
+      # nodePackages.insect
+      # rust-analyzer
+      # rnix-lsp
+      # ccls
+      # black
+      # gopls
+      # ccls
 
-      # reading packages
-      newsboat
+      # # reading packages
+      # newsboat
 
-      # keeb packages
-      qmk-udev-rules
-      via
-      qmk
+      # # keeb packages
+      # qmk-udev-rules
+      # via
+      # qmk
 
-      # dev packages
-      # gcc_multi
-      # avrlibc
-      # python2
-      starship
-      tealdeer
-      jupyter
-      ranger
-      stylua
-      cscope
-      delta
-      cargo
-      atuin
-      kitty
-      tmux
-      ruby
-      edir
-      curl
-      sbcl
-      zsh
-      exa
-      fzf
-      bat
-      fd
-      gh
-      jq
-      go
+      # # dev packages
+      # # gcc_multi
+      # # avrlibc
+      # # python2
+      # starship
+      # tealdeer
+      # jupyter
+      # ranger
+      # stylua
+      # cscope
+      # delta
+      # cargo
+      # atuin
+      # kitty
+      # tmux
+      # ruby
+      # edir
+      # curl
+      # sbcl
+      # zsh
+      # exa
+      # fzf
+      # bat
+      # fd
+      # gh
+      # jq
+      # go
 
-      # python packages
-      # (python39.withPackages (pp:
-      #   with pp; [
-      #     mysql-connector
-      #     pynvim
-      #     pandas
-      #     conda
-      #     requests
-      #     pip
-      #     i3ipc
-      #     ipython
-      #     dbus-python
-      #     html2text
-      #     keymapviz
-      #   ]))
+      # # python packages
+      # # (python39.withPackages (pp:
+      # #   with pp; [
+      # #     mysql-connector
+      # #     pynvim
+      # #     pandas
+      # #     conda
+      # #     requests
+      # #     pip
+      # #     i3ipc
+      # #     ipython
+      # #     dbus-python
+      # #     html2text
+      # #     keymapviz
+      # #   ]))
     ];
   };
 }
