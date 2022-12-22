b11d1341516e2a665fdfe66907d443dcd1edf1ec
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-12-22 08:15:14 -0500

    still trying

--- overlays/system.nix
+++ overlays/system.nix
@@ -1,4 +1,4 @@
 final: prev: {
-  libfprint-tod = final.callPackage ../../pkgs/libfprint-tod/default.nix { };
-  libfprint-tod-goodix = final.callPackage ../../pkgs/libfprint-tod-goodix/default.nix { };
+  libfprint-tod = final.callPackage ../../pkgs/libfprint-tod/default.nix {};
+  libfprint-tod-goodix = final.callPackage ../../pkgs/libfprint-tod-goodix/default.nix {};
 }
