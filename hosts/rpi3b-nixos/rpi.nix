a8301ae62410745d0d2fafe00dc629d18bdcf564
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-12-28 06:02:56 -0500

    disabling tailscale for now

--- hosts/rpi3b-nixos/rpi.nix
+++ hosts/rpi3b-nixos/rpi.nix
@@ -60,55 +60,55 @@
   system.stateVersion = config.system.nixos.release;
 
   # tailscale
-  services.tailscale.enable = true;
-
-  age.identityPaths = [
-    "/home/nixos/.ssh/id_ed25519"
-  ];
-  age.secrets.ts-xps17-nixos.file = ../../secrets/ts-rpi3b-nixos.age;
+  # services.tailscale.enable = true;
+  #
+  # age.identityPaths = [
+  #   "/home/nixos/.ssh/id_ed25519"
+  # ];
+  # age.secrets.ts-xps17-nixos.file = ../../secrets/ts-rpi3b-nixos.age;
 
   # create a oneshot job to authenticate to Tailscale
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
+  # systemd.services.tailscale-autoconnect = {
+  #   description = "Automatic connection to Tailscale";
+  #
+  #   # make sure tailscale is running before trying to connect to tailscale
+  #   after = ["network-pre.target" "tailscale.service"];
+  #   wants = ["network-pre.target" "tailscale.service"];
+  #   wantedBy = ["multi-user.target"];
+  #
+  #   # set this service as a oneshot job
+  #   serviceConfig.Type = "oneshot";
+  #
+  #   # have the job run this shell script
+  #   script = with pkgs; ''
+  #     # wait for tailscaled to settle
+  #     sleep 2
+  #
+  #     # check if we are already authenticated to tailscale
+  #     status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+  #     if [ $status = "Running" ]; then # if so, then do nothing
+  #       exit 0
+  #     fi
+  #
+  #     # otherwise authenticate with tailscale
+  #     ${tailscale}/bin/tailscale up --authkey=$(cat ${config.age.secrets.ts-rpi3b-nixos.path})
+  #   '';
+  # };
 
-    # make sure tailscale is running before trying to connect to tailscale
-    after = ["network-pre.target" "tailscale.service"];
-    wants = ["network-pre.target" "tailscale.service"];
-    wantedBy = ["multi-user.target"];
-
-    # set this service as a oneshot job
-    serviceConfig.Type = "oneshot";
-
-    # have the job run this shell script
-    script = with pkgs; ''
-      # wait for tailscaled to settle
-      sleep 2
-
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up --authkey=$(cat ${config.age.secrets.ts-rpi3b-nixos.path})
-    '';
-  };
-
-  networking.firewall = {
-    # warning: Strict reverse path filtering breaks Tailscale
-    # exit node use and some subnet routing setups.
-    checkReversePath = "loose";
-    # enable the firewall
-    enable = true;
-
-    # always allow traffic from your Tailscale network
-    trustedInterfaces = ["tailscale0"];
-
-    # allow the Tailscale UDP port through the firewall
-    allowedUDPPorts = [config.services.tailscale.port];
-
-    # allow you to SSH in over the public internet
-    allowedTCPPorts = [22];
-  };
+  # networking.firewall = {
+  #   # warning: Strict reverse path filtering breaks Tailscale
+  #   # exit node use and some subnet routing setups.
+  #   checkReversePath = "loose";
+  #   # enable the firewall
+  #   enable = true;
+  #
+  #   # always allow traffic from your Tailscale network
+  #   trustedInterfaces = ["tailscale0"];
+  #
+  #   # allow the Tailscale UDP port through the firewall
+  #   allowedUDPPorts = [config.services.tailscale.port];
+  #
+  #   # allow you to SSH in over the public internet
+  #   allowedTCPPorts = [22];
+  # };
 }
