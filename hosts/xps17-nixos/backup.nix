932cf7c967e2990e4786cb218b17c69e22890985
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-01-03 18:01:30 -0500

    trying to make borg work

--- hosts/xps17-nixos/backup.nix
+++ hosts/xps17-nixos/backup.nix
@@ -1,29 +1,14 @@
+# https://xeiaso.net/blog/borg-backup-2021-01-09
 {
   config,
   pkgs,
   agenix,
   ...
 }: {
-  age.identityPaths = [
-    "/home/morp/.ssh/id_ed25519"
-  ];
-  age.secrets.borgbackup-xps17-nixos.file = ../../secrets/borgbackup-xps17-nixos.age;
-
   services.borgbackup.jobs."borgbase" = {
-    paths = [
-      "/home/morp/Dropbox/"
-    ];
-    exclude = [
-      # "/var/lib/docker"
-      # "**/target"
-      # "/home/*/go/bin"
-    ];
-    # repo = "o6h6zl22@o6h6zl22.repo.borgbase.com:repo";
-    repo = "ssh://a1gm1p14@a1gm1p14.repo.borgbase.com/./repo";
-    encryption = {
-      mode = "repokey-blake2";
-      passCommand = "cat ${config.age.secrets.borgbackup-xps17-nixos.path}";
-    };
+    paths = ["/home/morp/Dropbox/"];
+    repo = "a1gm1p14@a1gm1p14.repo.borgbase.com:repo";
+    encryption.mode = "none";
     environment.BORG_RSH = "ssh -i /home/morp/.ssh/id_ed25519";
     compression = "auto,zstd";
     startAt = "daily";
