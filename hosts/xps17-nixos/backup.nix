41ef8e0d735925d75875d720a56cc8cc235e0638
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-01-08 21:20:07 -0500

    few changes

--- hosts/xps17-nixos/backup.nix
+++ hosts/xps17-nixos/backup.nix
@@ -6,7 +6,7 @@
 }: {
   # use agenix for passCommand
   age.identityPaths = [
-    "/home/morp/.ssh/id_ed25519_borgbase"
+    "/root/.ssh/id_ed25519_borgbase"
   ];
   age.secrets.borgbackup-xps17-nixos.file = ../../secrets/borgbackup-xps17-nixos.age;
   services.borgbackup.jobs = {
@@ -18,7 +18,7 @@
         mode = "repokey-blake2";
         passCommand = "cat ${config.age.secrets.borgbackup-xps17-nixos.path}";
       };
-      environment = {BORG_RSH = "ssh -i ~/.ssh/id_ed25519_borgbase ";};
+      environment = {BORG_RSH = "ssh -i /root/.ssh/id_ed25519_borgbase ";};
       compression = "auto,zstd";
       startAt = "daily";
     };
