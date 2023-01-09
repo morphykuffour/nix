60260781d6dce125c2a0469886dd3dabbcc09f0b
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-01-09 15:09:22 -0500

    few changes

--- hosts/xps17-nixos/backup.nix
+++ hosts/xps17-nixos/backup.nix
@@ -11,13 +11,17 @@
   age.secrets.borgbackup-xps17-nixos.file = ../../secrets/borgbackup-xps17-nixos.age;
   services.borgbackup.jobs = {
     dropbox_backup = {
+      user = "root";
       paths = ["/home/morp/Dropbox/"];
       exclude = ["'**/.cache'"];
       repo = "r0el6zc7@r0el6zc7.repo.borgbase.com:repo";
-      encryption = {
-        mode = "repokey-blake2";
-        passCommand = "cat ${config.age.secrets.borgbackup-xps17-nixos.path}";
-      };
+      encryption.mode = "none";
+      # encryption = {
+      #   mode = "repokey-blake2";
+      #   # passphrase = "cat ${config.age.secrets.borgbackup-xps17-nixos.path}";
+      #   passCommand = "cat ${config.age.secrets.borgbackup-xps17-nixos.path}";
+      #   # passCommand = "cat ../../../secrets/passphrase";
+      # };
       environment = {BORG_RSH = "ssh -i /root/.ssh/id_ed25519_borgbase ";};
       compression = "auto,zstd";
       startAt = "daily";
