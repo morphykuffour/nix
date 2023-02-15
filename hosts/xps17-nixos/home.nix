50816125ad67ba95c4de8aa439a17373322f0b09
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-02-14 19:27:31 -0500

    few changes

--- hosts/xps17-nixos/home.nix
+++ hosts/xps17-nixos/home.nix
@@ -40,6 +40,15 @@
         };
       };
     };
+    vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        dracula-theme.theme-dracula
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+        ms-toolsai.jupyter
+      ];
+    };
   };
 
   nixpkgs = {
