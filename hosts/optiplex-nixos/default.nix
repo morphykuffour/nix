1071b518b6b58f799f8ad4b29c7f41af9935952a
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-02-20 23:33:23 -0500

    few changes

--- hosts/optiplex-nixos/default.nix
+++ hosts/optiplex-nixos/default.nix
@@ -1,10 +1,38 @@
 {
-  config,
-  pkgs,
+  home-manager,
+  alejandra,
+  agenix,
+  self,
+  nixpkgs,
+  inputs,
   user,
+  overlays,
   ...
-}: {
-  imports = [
+}:
+nixpkgs.lib.nixosSystem {
+  system = "x86_64-linux";
+  modules = [
+    # ./hosts/optiplex-nixos
     ./configuration.nix
+    agenix.nixosModules.default
+    {
+      environment.systemPackages = [
+        alejandra.defaultPackage.x86_64-linux
+        agenix.packages.x86_64-linux.default
+        # neovim.packages.x86_64-linux.neovim
+      ];
+    }
+    home-manager.nixosModules.home-manager
+    {
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.${user}.imports = [./hosts/xps17-nixos/home.nix];
+        # extraSpecialArgs = {
+        #   plover = inputs.plover.packages."x86_64-linux".plover;
+        # };
+      };
+    }
   ];
+  specialArgs = inputs;
 }
