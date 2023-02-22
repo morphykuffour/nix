54d7ab5bf148f2a3a1fc75409699a1d5f5ed02ed
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-02-22 10:38:22 -0500

    few changes

--- hosts/xps17-nixos/configuration.nix
+++ hosts/xps17-nixos/configuration.nix
@@ -248,28 +248,29 @@ in {
       overrideFolders = true;
       devices = {
         "xps17-nixos" = {id = "44LYB6O-ELZWVNP-5R576R3-MRD3MM2-FXORGWG-WRC26ZQ-JAMWKRS-5SCNUAY";};
-        "rpi3b-ubuntu" = {id = "TTEQED5-YB5HDQQ-4OYRRUE-PQMO7XF-TWCNSQ7-4SFRM5X-N6C3IBY-ELN2XQV";};
+        # "rpi3b-ubuntu" = {id = "TTEQED5-YB5HDQQ-4OYRRUE-PQMO7XF-TWCNSQ7-4SFRM5X-N6C3IBY-ELN2XQV";};
         "macmini-darwin" = {id = "OK4365M-ZZC4CDT-A6W2YF2-MPIX3GR-FYZIWWJ-5QS6RYM-5KYU35K-SLYBHQO";};
         "workstation-windows" = {id = "OT562TI-J4NCYP6-7SCXJL6-PWDVBGX-EJA5G7S-3Q4G4TG-UR7RN3F-V3OVAAH";};
       };
 
       folders = {
-        # "Dropbox" = {
-        #   path = "/home/morp/Dropbox";
-        #   devices = ["xps17-nixos" "rpi3b-ubuntu" "macmini-darwin" "workstation-windows"];
-        #   versioning = {
-        #     type = "staggered";
-        #     params = {
-        #       cleanInterval = "3600";
-        #       maxAge = "15768000";
-        #     };
-        #   };
-        # };
+        "Dropbox" = {
+          path = "/home/morp/Dropbox";
+          id = "Dropbox";
+          devices = ["xps17-nixos" "macmini-darwin" "workstation-windows"];
+          versioning = {
+            type = "staggered";
+            params = {
+              cleanInterval = "3600";
+              maxAge = "15768000";
+            };
+          };
+        };
 
         "Org" = {
           path = "/home/morp/Org/";
-          id = "prsu2-hrpwq";
-          devices = ["xps17-nixos" "rpi3b-ubuntu" "macmini-darwin" "workstation-windows"];
+          id = "Org";
+          devices = ["xps17-nixos" "macmini-darwin" "workstation-windows"];
           versioning = {
             type = "staggered";
             params = {
@@ -281,7 +282,8 @@ in {
 
         "iCloud" = {
           path = "/home/morp/iCloud/";
-          devices = ["xps17-nixos" "rpi3b-ubuntu" "macmini-darwin" "workstation-windows"];
+          id = "iCloud";
+          devices = ["xps17-nixos" "macmini-darwin" "workstation-windows"];
           versioning = {
             type = "staggered";
             params = {
