38920528b57c0c41c093f7fa8c4c8c2adb455b2b
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-03-20 10:51:37 -0400

    few changes

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -1,15 +1,23 @@
-{ inputs, pkgs, lib, config, ... }:
-
-with lib;
-let cfg = config.modules.hyprland;
-
+{
+  inputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.modules.hyprland;
 in {
-    options.modules.hyprland= { enable = mkEnableOption "hyprland"; };
-    config = mkIf cfg.enable {
-	home.packages = with pkgs; [
-	    wofi swaybg wlsunset wl-clipboard hyprland
-	];
+  options.modules.hyprland = {enable = mkEnableOption "hyprland";};
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      wofi
+      swaybg
+      wlsunset
+      wl-clipboard
+      hyprland
+    ];
 
-        home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
-    };
+    home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+  };
 }
