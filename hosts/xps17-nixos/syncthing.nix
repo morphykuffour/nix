3e20c111a5df6a8b8022418818109d16800b5212
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-06-21 14:13:06 -0400

    added new device identification

--- hosts/xps17-nixos/syncthing.nix
+++ hosts/xps17-nixos/syncthing.nix
@@ -15,7 +15,7 @@
     overrideDevices = true;
     overrideFolders = true;
     devices = {
-      "xps17-nixos" = {id = "44LYB6O-ELZWVNP-5R576R3-MRD3MM2-FXORGWG-WRC26ZQ-JAMWKRS-5SCNUAY";};
+      "xps17-nixos" = {id = "7AB6YRE-AYP7FOX-7PWJLDV-TRMJ6B6-S5OGOD3-GQ5HFPT-DCPMHYS-IBQKJQK"; };
       "ubuntu" = {id = "TTEQED5-YB5HDQQ-4OYRRUE-PQMO7XF-TWCNSQ7-4SFRM5X-N6C3IBY-ELN2XQV";};
       "macmini-darwin" = {id = "OK4365M-ZZC4CDT-A6W2YF2-MPIX3GR-FYZIWWJ-5QS6RYM-5KYU35K-SLYBHQO";};
       "workstation-windows" = {id = "OT562TI-J4NCYP6-7SCXJL6-PWDVBGX-EJA5G7S-3Q4G4TG-UR7RN3F-V3OVAAH";};
