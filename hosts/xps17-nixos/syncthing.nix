8c657c3d2577fc65a9783c4edd63f09f2244fa5e
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-09-30 13:09:02 -0400

    trying to fix building issue

--- hosts/xps17-nixos/syncthing.nix
+++ hosts/xps17-nixos/syncthing.nix
@@ -14,14 +14,14 @@
     guiAddress = "127.0.0.1:8384";
     overrideDevices = true;
     overrideFolders = true;
-    devices = {
+    settings.devices = {
       "xps17-nixos" = {id = "7AB6YRE-AYP7FOX-7PWJLDV-TRMJ6B6-S5OGOD3-GQ5HFPT-DCPMHYS-IBQKJQK";};
       "ubuntu" = {id = "TTEQED5-YB5HDQQ-4OYRRUE-PQMO7XF-TWCNSQ7-4SFRM5X-N6C3IBY-ELN2XQV";};
       "macmini-darwin" = {id = "OK4365M-ZZC4CDT-A6W2YF2-MPIX3GR-FYZIWWJ-5QS6RYM-5KYU35K-SLYBHQO";};
       # "workstation-windows" = {id = "OT562TI-J4NCYP6-7SCXJL6-PWDVBGX-EJA5G7S-3Q4G4TG-UR7RN3F-V3OVAAH";};
     };
 
-    folders = {
+    settings.folders = {
       "Org" = {
         path = "/home/morph/Org/";
         id = "Org";
