82efd78aef86e66d677a22058e0ef182bde050fb
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-11-11 16:27:25 -0500

    replacing protonvpn with mullvad vpn

---
+++ modules/mullvad.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  environment.systemPackages = [pkgs.mullvad-vpn pkgs.mullvad];
+  services.mullvad-vpn = {
+    enable = true;
+  };
+
+  systemd.services."mullvad-daemon".postStart = let
+    mullvad = config.services.mullvad-vpn.package;
+  in ''
+    while ! ${mullvad}/bin/mullvad status >/dev/null; do sleep 1; done
+    ${mullvad}/bin/mullvad auto-connect set on
+    ${mullvad}/bin/mullvad tunnel ipv6 set on
+    ${mullvad}/bin/mullvad set default \
+        --block-ads --block-trackers --block-malware
+  '';
+}
