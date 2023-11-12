899f4e36e2ffdda9b449eccf4817acbf8bb68842
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-11-12 12:46:09 -0500

    more cleanup

--- hosts/xps17-nixos/overlays/emacs.nix
+++
@@ -1,5 +0,0 @@
-with rec {
-  emacs-overlay = import (builtings.fetchTarball {url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;});
-  pkgs = import <nixpkgs> {overlays = [emacs-overlay];};
-};
-  pkgs.emacsGcc
