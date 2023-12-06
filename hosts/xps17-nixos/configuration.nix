d858d4cd8934ceff17f663213b3367528ab242a8
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-12-06 11:21:54 -0500

    adding pipewire for sound

--- hosts/xps17-nixos/configuration.nix
+++ hosts/xps17-nixos/configuration.nix
@@ -59,6 +59,15 @@
     optimise.automatic = true;
   };
 
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
   services = {
     emacs = {
       # package = pkgs.emacs-unstable;
