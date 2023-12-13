d26a063b6f3725f57ef103c16f852d0cd6a032b7
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-12-13 12:25:01 -0500

    cleanup of code

--- hosts/xps17-nixos/restic.nix
+++ hosts/xps17-nixos/restic.nix
@@ -38,54 +38,5 @@
         "--keep-monthly 12"
       ];
     };
-
-    # cloud-system_drive_backup-xps17-nixos = {
-    #   exclude = [
-    #     "/home/*/.cache"
-    #   ];
-    #   initialize = true;
-    #   passwordFile = config.age.secrets.b2-backup-xps17-nixos.path;
-    #   paths = [
-    #     "/home"
-    #   ];
-    #   repository = "b2:xps17-nixos-backup";
-    #   timerConfig = {
-    #     OnUnitActiveSec = "1d";
-    #   };
-
-    #   # keep 7 daily, 5 weekly, and 10 annual backups
-    #   pruneOpts = [
-    #     "--keep-daily 7"
-    #     "--keep-weekly 5"
-    #     "--keep-yearly 10"
-    #   ];
-    # };
-
-    # Sync = {
-    #   initialize = true;
-    #   passwordFile = config.age.secrets.b2-backup-xps17-nixos.path;
-    #   paths = [
-    #     "/home/morph/Org"
-    #     "/home/morph/iCloud"
-    #   ];
-    #   repository = "b2:xps17-nixos-backup";
-    #   timerConfig = {
-    #     OnUnitActiveSec = "1d";
-    #   };
-
-    #   # keep 7 daily, 5 weekly, and 10 annual backups
-    #   pruneOpts = [
-    #     "--keep-daily 7"
-    #     "--keep-weekly 5"
-    #     "--keep-yearly 10"
-    #   ];
-    # };
   };
-
-  # systemd.services.restic-backups-morph = {
-  #   environment = {
-  #     B2_ACCOUNT_ID = "my_account_id_abc123";
-  #     B2_ACCOUNT_KEY = "my_account_key_def456";
-  #   };
-  # };
 }
