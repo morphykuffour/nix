18c9a0753494019e590eecd6f3ddc9f95b4c74f2
Morphy Kuffour <morphy.kuffour@uconn.edu>
2024-11-23 14:21:57 -0500

    adding configuration for t480

---
+++ hosts/t480-nixos/configuration.nix
@@ -0,0 +1,311 @@
+{
+  inputs,
+  config,
+  current,
+  lib,
+  pkgs,
+  home-manager,
+  agenix,
+  user,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    # ./keyd.nix
+    # ./tailscale.nix
+    # ./syncthing.nix
+    # ./restic.nix
+    # ./dslr.nix
+  ];
+
+  # Bootloader zfs specific
+  boot.supportedFilesystems = [ "zfs" ];
+  networking.hostId = "944dfacf";
+
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub = {
+    enable = true;
+    version =2;
+    device = "nodev";
+    efiSupport = true;
+    enableCryptodisk = true;
+    extraEntries = ''
+      menuentry "Windows" {
+        insmod part_gpt
+        insmod fat
+        insmod search_fs_uuid
+        insmod chain
+        search --fs-uuid --set=root $FS_UUID
+        chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+      }
+    '';
+  };
+
+  boot.initrd.luks.devices = {
+   root = {
+     device = "/dev/disk/by-uuid/08ecff77-0009-4995-8f83-17ff64ee6bd4"; ## Use blkid to find this UUID 
+     # Required even if we're not using LVM
+     preLVM = true;
+   };
+  };
+
+  # Enable networking
+  networking = {
+    hostName = "t480-nixos";
+    networkmanager.enable = true;
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # firewall = {
+    #   enable = true;
+    #   trustedInterfaces = [ "tailscale0" ];
+    # };
+  };
+
+  # Enable tailscale Mesh VPN
+  # services.tailscale.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  nix = {
+    extraOptions = "experimental-features = nix-command flakes";
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 14d";
+    };
+    optimise.automatic = true;
+  };
+
+  security.rtkit.enable = true;
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  services = {
+    emacs = {
+      package = pkgs.emacs;
+      enable = true;
+      install = true;
+    };
+
+    blueman.enable = true;
+
+    xserver = {
+      libinput.enable = true;
+      enable = true;
+
+      layout = "us";
+      xkbVariant = "";
+      desktopManager = {
+        mate = {
+          enable = true;
+          # excludePackages = [ pkgs.mate.mate-terminal pkgs.mate.pluma ];
+        };
+      };
+
+      displayManager = {
+        startx.enable = false;
+        # sddm = {
+        #   enable = true;
+        # };
+        gdm = {
+          enable = true;
+          wayland = false;
+        };
+        autoLogin = {
+          enable = false;
+          user = "morph";
+        };
+      };
+
+      windowManager = {
+        i3 = {
+          enable = true;
+          package = pkgs.i3-gaps;
+        };
+      };
+    };
+
+    # printing.enable = true;
+
+    avahi = {
+      nssmdns = true;
+      enable = true;
+      openFirewall = true;
+      publish = {
+        enable = true;
+        userServices = true;
+        domain = true;
+      };
+      allowInterfaces = ["wlp0s20f3" "tailscale0"];
+    };
+  };
+
+  users.users.morph = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    description = "Morphy Kuffour";
+    extraGroups = ["networkmanager" "wheel" "libvirtd"];
+    packages = with pkgs; [];
+  };
+
+  programs = {
+    waybar.enable = true;
+    zsh.enable = true;
+    mtr.enable = true;
+    autojump.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-volman
+      ];
+    };
+  };
+
+  # vms
+  # virtualisation.libvirtd.enable = true;
+  # programs.virt-manager.enable = true;
+  # virtualisation.docker = {
+  #   enable = true;
+  #   rootless = {
+  #     enable = true;
+  #     setSocketVariable = true;
+  #   };
+  # };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system = {
+    # stateVersion = config.system.nixos.release;
+    stateVersion = "23.05"; # Did you read the comment?
+    autoUpgrade = {
+      enable = true;
+      allowReboot = true;
+    };
+  };
+
+  # sudoless
+  # security.sudo.wheelNeedsPassword = false;
+
+  environment.interactiveShellInit = ''
+    alias c99='gcc'
+  '';
+
+  environment.systemPackages = with pkgs; [
+    wget
+    xclip
+    git
+    stow
+    brave
+    sqlite
+    unzip
+    coreutils
+    binutils
+    gcc
+    flameshot
+    sxiv
+    gnumake
+    xorg.xbacklight
+    autorandr
+    xdotool
+    xdg-user-dirs
+    pciutils
+    usbutils
+    ventoy-bin
+    rustup
+    rustc
+    brightnessctl
+    xdragon
+    keyd
+    gnome.dconf-editor
+    mate.mate-power-manager
+    mate.mate-media
+    orchis-theme
+    tela-circle-icon-theme
+    docker
+    libreoffice
+    # reptyr
+    wireshark
+    tshark
+    procps
+    zsync
+    cdrkit
+    sqlitebrowser
+    bashmount
+    # vagrant
+    grub2
+    qemu
+    libvirt
+    virt-manager
+    spice-gtk
+    quickemu
+    samba
+    OVMF
+    gdb
+    libinput-gestures
+    wmctrl
+    fwupd
+    # mysql80
+    # dbeaver
+    dig
+    psmisc
+    # discord
+    # mycli
+    grc
+    cmake
+    ninja
+    clang
+    yarn
+    firefox
+    ffmpeg-full
+    vim
+    wireguard-tools
+    libtool
+    libvterm
+    os-prober
+    kitty
+    age
+    rage
+    uxplay
+    arandr
+    libclang
+    libstdcxx5
+    ctags
+    zsh-completions
+    networkmanager
+    networkmanagerapplet
+    dmenu
+    avahi
+    eza
+    discord
+    gimp
+    # mullvad
+  ];
+}
---
+++ hosts/t480-nixos/home.nix
@@ -0,0 +1,133 @@
+{
+  inputs,
+  config,
+  current,
+  pkgs,
+  lib,
+  plover,
+  user,
+  agenix,
+  ...
+}: {
+  imports = [
+    ../../modules/i3.nix
+    ../../modules/pass.nix
+    ../../modules/fonts.nix
+    ../../modules/zathura
+    ../../modules/lf
+    # ../../modules/nvim.nix
+    ../../modules/redshift.nix
+    # ../../modules/picom.nix
+  ];
+
+  services.clipmenu.enable = true;
+  programs = {
+    home-manager = {
+      enable = true;
+    };
+    # obs-studio = {
+    #   enable = true;
+    #   plugins = with pkgs.obs-studio-plugins; [
+    #     wlrobs
+    #     obs-gstreamer
+    #   ];
+    # };
+    vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        dracula-theme.theme-dracula
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+        ms-toolsai.jupyter
+      ];
+    };
+  };
+
+  dconf.settings = {
+    "org/virt-manager/virt-manager/connections" = {
+      autoconnect = ["qemu:///system"];
+      uris = ["qemu:///system"];
+    };
+  };
+
+  nixpkgs = {
+    config.allowUnfree = true;
+  };
+
+  home = {
+    username = "morph";
+    homeDirectory = "/home/morph";
+    stateVersion = "22.05";
+    packages = with pkgs; [
+      brave
+      tmux
+      zsh
+      starship
+      eza
+      bat
+      tealdeer
+      fd
+      gh
+      clipmenu
+      delta
+      jupyter
+      ruby
+      edir
+      ranger
+      stylua
+      cargo
+      jq
+      curl
+      fzf
+      sbcl
+      neomutt
+      mu
+      isync
+      msmtp
+      pass
+      eva
+      mcfly
+      hexyl
+      ripgrep
+      autojump
+      pandoc
+      croc
+      zathura
+      go
+      feh
+      tree-sitter
+      # nodePackages.insect
+      file
+      newsboat
+      neovim
+      texlive.combined.scheme-full
+      qmk
+      qmk-udev-rules
+      p7zip
+      ruby
+      zip
+
+      # python packages
+      # (python39.withPackages (pp:
+      #   with pp; [
+      #     pynvim
+      #     # pandas
+      #     # reticulate needs conda
+      #     # conda
+      #     # requests
+      #     pip
+      #     i3ipc
+      #     ipython
+      #     dbus-python
+      #     html2text
+      #     keymapviz
+      #     # mysql-connector
+      #     # pipx
+      #     # pyqt5
+      #     ueberzug
+      #   ]))
+      # https://stackoverflow.com/questions/52941074/in-nixos-how-can-i-resolve-a-collision
+      # ]).override (args: { ignoreCollisions = true; }))
+    ];
+  };
+}
