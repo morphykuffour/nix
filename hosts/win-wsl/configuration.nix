90f436f1dae11825c2863a4519ad11e8097854ef
Morphy Kuffour <morphy.kuffour@uconn.edu>
2025-02-14 00:22:25 -0500

    adding ollama to win-wsl

--- hosts/win-wsl/configuration.nix
+++ hosts/win-wsl/configuration.nix
@@ -46,6 +46,30 @@
     # And then enable them for the relevant users:
     # systemctl --user enable auto-fix-vscode-server.service
     # vscode-server.enable = true;
+    ollama = {
+      # package = pkgs.unstable.ollama; # If you want to use the unstable channel package for example
+      enable = true;
+      acceleration = "cuda"; # Or "rocm"
+      # environmentVariables = { # I haven't been able to get this to work myself yet, but I'm sharing it for the sake of completeness
+        # HOME = "/home/ollama";
+        # OLLAMA_MODELS = "/home/ollama/models";
+        # OLLAMA_HOST = "0.0.0.0:11434"; # Make Ollama accesible outside of localhost
+        # OLLAMA_ORIGINS = "http://localhost:8080,http://192.168.0.10:*"; # Allow access, otherwise Ollama returns 403 forbidden due to CORS
+      #};
+    };
+
+    open-webui = {
+      enable = true;
+      environment = {
+        ANONYMIZED_TELEMETRY = "False";
+        DO_NOT_TRACK = "True";
+        SCARF_NO_ANALYTICS = "True";
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+        OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+      };
+    };
+
+
   };
 
   wsl = {
@@ -168,7 +192,7 @@
     fzf
     mcfly
     cargo
-
+    oterm
     # R packages for data science
     # rstudio
     # (pkgs.rWrapper.override {
