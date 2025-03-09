0e309f091527c34f55c7dedc70043ad257422019
Morphy Kuffour <morphy.kuffour@uconn.edu>
2025-03-09 18:56:46 -0400

    finally got tailscale updated to latest

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -1,23 +1,12 @@
 { config, lib, pkgs, ... }:
 
-let
-  latestGo = pkgs.go_1_24; # Fetch the latest Go version
-  tailscalePkg = pkgs.tailscale.overrideAttrs (old: {
-    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ latestGo ];
-  });
-in
 {
-  options = {
-    services.tailscale.package = lib.mkOption {
-      type = lib.types.package;
-      default = tailscalePkg;
-      description = "The Tailscale package to install.";
-    };
-  };
-
   config = {
     environment.systemPackages = [ config.services.tailscale.package ];
 
-    services.tailscale.enable = true;
+    services.tailscale = {
+      enable = true;
+      package = lib.mkForce pkgs.tailscale;
+    };
   };
 }
