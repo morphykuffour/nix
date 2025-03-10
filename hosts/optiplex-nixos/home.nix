3b6b184556eddcb17f37e0560d3ebc7df100b6b2
Morphy Kuffour <morphy.kuffour@uconn.edu>
2025-03-09 21:11:06 -0400

    few changes

--- hosts/optiplex-nixos/home.nix
+++ hosts/optiplex-nixos/home.nix
@@ -122,14 +122,14 @@
       # termite
       # plover
       tree-sitter
-      rnix-lsp
+      # rnix-lsp
       gopls
       ccls
       fpp
       # tree-sitter-grammars.tree-sitter-markdown
       # sumneko-lua-language-server
       # nodePackages.typescript-language-server
-      nodePackages.insect
+      # nodePackages.insect
       # nodePackages.mermaid-cli
       # nodePackages.bash-language-server
       # nodePackages.pyright
