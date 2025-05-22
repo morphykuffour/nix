5250344be7a694d8e7030a4e50f0babdcfb00b99
Morphy Kuffour <morphy.kuffour@uconn.edu>
2025-05-21 20:26:51 -0400

    adding xps17 kanata config

---
+++ modules/kanata/default.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let 
+  kanata_config = builtins.readfile ./xps17.kbd;
+in {
+  # Enable the uinput module
+  boot.kernelModules = [ "uinput" ];
+
+  # Enable uinput
+  hardware.uinput.enable = true;
+
+  # Set up udev rules for uinput
+  services.udev.extraRules = ''
+    KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+  '';
+
+  # Ensure the uinput group exists
+  users.groups.uinput = { };
+
+  # Add the Kanata service user to necessary groups
+  systemd.services.kanata-internalKeyboard.serviceConfig = {
+    SupplementaryGroups = [
+      "input"
+      "uinput"
+    ];
+  };
+
+  services.kanata = {
+    enable = true;
+    keyboards = {
+      internalKeyboard = {
+        devices = [
+          "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+        ];
+        extraDefCfg = "process-unmapped-keys yes";
+        config = ''${kanata_config}'';
+      };
+    };
+  };
+}
