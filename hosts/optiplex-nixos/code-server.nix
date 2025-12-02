af5c88a299a72da049ebdd11a0216c7e63af13eb
Morphy Kuffour <morphy.kuffour@uconn.edu>
2025-12-01 19:53:05 -0500

    using caddy now

--- hosts/optiplex-nixos/code-server.nix
+++ hosts/optiplex-nixos/code-server.nix
@@ -8,29 +8,36 @@
     enable = true;
     user = "morph";
     group = "users";
-    host = "127.0.0.1"; # Bind to localhost only, Tailscale serve will handle external access
+    host = "127.0.0.1";
     port = 8081; # Using 8081 since qBittorrent uses 8080
     auth = "none"; # Tailscale provides authentication
     extraEnvironment = {
-      # Prevent code-server from generating a password
       CS_DISABLE_GETTING_STARTED_OVERRIDE = "1";
     };
-    extraArguments = [
-      "--proxy-domain=optiplex-nixos.tailc585e.ts.net/code-server"
-    ];
   };
 
-  # Advertise code-server as a Tailscale service
+  # Caddy as a local reverse proxy to handle path rewriting
+  services.caddy = {
+    enable = true;
+    # Run on a different port since Tailscale is using 443
+    virtualHosts."127.0.0.1:8082".extraConfig = ''
+      handle_path /code-server* {
+        reverse_proxy 127.0.0.1:8081
+      }
+    '';
+  };
+
+  # Advertise Caddy (with code-server behind it) via Tailscale
   systemd.services.tailscale-serve-code-server = {
-    description = "Advertise code-server on Tailscale";
-    after = ["tailscale.service" "code-server.service"];
-    wants = ["tailscale.service" "code-server.service"];
+    description = "Advertise code-server on Tailscale via Caddy";
+    after = ["tailscale.service" "code-server.service" "caddy.service"];
+    wants = ["tailscale.service" "code-server.service" "caddy.service"];
     wantedBy = ["multi-user.target"];
 
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
-      ExecStart = "${config.services.tailscale.package}/bin/tailscale serve --bg --https=443 --set-path=/code-server http://127.0.0.1:8081";
+      ExecStart = "${config.services.tailscale.package}/bin/tailscale serve --bg --https=443 --set-path=/code-server http://127.0.0.1:8082/code-server";
       ExecStop = "${config.services.tailscale.package}/bin/tailscale serve --https=443 --set-path=/code-server off";
     };
   };
