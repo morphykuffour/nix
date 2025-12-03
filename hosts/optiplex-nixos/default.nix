cea2ffebc289c08e2d72459c797bde9b7b4ce52f
Morphy Kuffour <morphy.kuffour@uconn.edu>
2025-12-02 21:39:07 -0500

    moved code to it's own module

--- hosts/optiplex-nixos/default.nix
+++ hosts/optiplex-nixos/default.nix
@@ -17,75 +17,7 @@ nixpkgs.lib.nixosSystem {
     ../../modules/tailscale
     agenix.nixosModules.default
     home-manager.nixosModules.home-manager
-
-    # vertd module with package override for build dependencies
-    # The vertd flake has a bug where buildDepsOnly doesn't get the nativeBuildInputs
-    # So we need to override the package to fix the cargo dependency build
-    ({pkgs, lib, ...}: let
-      crane = inputs.vertd.inputs.crane.mkLib pkgs;
-      src = crane.cleanCargoSource inputs.vertd;
-
-      commonArgs = {
-        inherit src;
-        strictDeps = true;
-        # Add build dependencies - openssl.dev in nativeBuildInputs so setup hooks run
-        nativeBuildInputs = [
-          pkgs.pkg-config
-          pkgs.openssl.dev
-        ];
-        buildInputs = [
-          pkgs.openssl
-        ];
-      };
-
-      # Build cargo dependencies with proper inputs
-      # Override the derivation to ensure environment variables are available
-      cargoArtifacts = (crane.buildDepsOnly commonArgs).overrideAttrs (oldAttrs: {
-        # Add environment variables to the build environment
-        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-        OPENSSL_DIR = "${pkgs.openssl.dev}";
-        OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
-        OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
-        
-        # Ensure preBuild runs and sets up the environment
-        preBuild = (oldAttrs.preBuild or "") + ''
-          export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
-          export OPENSSL_DIR="${pkgs.openssl.dev}"
-          export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
-          export OPENSSL_INCLUDE_DIR="${pkgs.openssl.dev}/include"
-          export PATH="${lib.makeBinPath [ pkgs.pkg-config ]}:$PATH"
-        '';
-      });
-
-      # Build vertd with the artifacts
-      vertd-fixed = crane.buildPackage (commonArgs // {
-        inherit cargoArtifacts;
-        nativeBuildInputs = commonArgs.nativeBuildInputs ++ [
-          pkgs.makeWrapper
-        ];
-        # Set environment variables for the final build too
-        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-        OPENSSL_DIR = "${pkgs.openssl.dev}";
-        OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
-        OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
-        postFixup = ''
-          wrapProgram $out/bin/vertd --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ pkgs.libGL ]}"
-        '';
-        meta = {
-          description = "VERT's solution to video conversion";
-          homepage = "https://github.com/vert-sh/vertd";
-          license = lib.licenses.gpl3;
-          platforms = lib.platforms.linux;
-          mainProgram = "vertd";
-        };
-      });
-    in {
-      nixpkgs.overlays = [
-        (final: prev: {
-          vertd = vertd-fixed;
-        })
-      ];
-    })
+    ./vertd-package.nix
     inputs.vertd.nixosModules.default
     ./vertd.nix
     {
