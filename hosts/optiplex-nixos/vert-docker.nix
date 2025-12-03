84874930f4064ab2058ccf5a532d76038d0f37ff
Morphy Kuffour <morphy.kuffour@uconn.edu>
2025-12-03 01:48:34 -0500

    vert is finally working

--- hosts/optiplex-nixos/vert-docker.nix
+++ hosts/optiplex-nixos/vert-docker.nix
@@ -2,6 +2,11 @@
 
 {
   # VERT file converter Docker container
+  # Note: Pre-built images have environment vars baked in at build time
+  # To connect to local vertd, we need to either:
+  # 1. Build custom image with PUB_VERTD_URL pointing to local backend
+  # 2. Use Tailscale to expose vertd and configure it in the web UI
+
   virtualisation.oci-containers.containers.vert = {
     image = "ghcr.io/vert-sh/vert:latest";
     autoStart = true;
@@ -17,4 +22,7 @@
   # Enable Docker/OCI containers
   virtualisation.docker.enable = true;
   virtualisation.oci-containers.backend = "docker";
+
+  # Ensure git is available for building custom VERT image if needed
+  environment.systemPackages = with pkgs; [ git ];
 }
