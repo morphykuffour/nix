8696600ca49fff3755c2a59f36b46e06468e316b
Morphy Kuffour <your-email@example.com>
2025-12-07 11:15:07 -0500

    few changes

--- hosts/xps17-nixos/syncthing.nix
+++ hosts/xps17-nixos/syncthing.nix
@@ -17,13 +17,14 @@
     settings.devices = {
       "macmini-darwin" = {id = "OK4365M-ZZC4CDT-A6W2YF2-MPIX3GR-FYZIWWJ-5QS6RYM-5KYU35K-SLYBHQO";};
       "optiplex-nixos" = {id = "ZXN6ICB-6DIAJ3E-LSOBFNF-VBAPZOA-DYHC2CE-TE4KYD5-CGHRJWL-GCFP3AJ";};
+      "desktop-bqtgj9g" = {id = "TWWE3ND-3VVVCVF-BRLFCJN-FY52C2R-6CKQFBX-NLABCDI-H2BNSSU-4P6OOAP";};
     };
 
     settings.folders = {
       "Org" = {
         path = "/home/morph/Org/";
         id = "Org";
-        devices = ["optiplex-nixos" "macmini-darwin"];
+        devices = ["desktop-bqtgj9g" "optiplex-nixos" "macmini-darwin"];
         versioning = {
           type = "staggered";
           params = {
@@ -33,18 +34,18 @@
         };
       };
 
-      "iCloud" = {
-        path = "/home/morph/iCloud/";
-        id = "iCloud";
-        devices = ["optiplex-nixos" "macmini-darwin"];
-        versioning = {
-          type = "staggered";
-          params = {
-            cleanInterval = "3600";
-            maxAge = "15768000";
-          };
-        };
-      };
+      # "Sync" = {
+      #   path = "/home/morph/Sync/";
+      #   id = "Sync";
+      #   devices = ["desktop-bqtgj9g" "optiplex-nixos" "macmini-darwin"];
+      #   versioning = {
+      #     type = "staggered";
+      #     params = {
+      #       cleanInterval = "3600";
+      #       maxAge = "15768000";
+      #     };
+      #   };
+      # };
     };
   };
 }
