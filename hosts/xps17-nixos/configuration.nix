eb95c23d5093355f7ac761df40ae89ebb6b33071
Morphy Kuffour <morpkuff@gmail.com>
2025-12-09 15:43:24 -0500

    Add XDG portals support for Wayland applications in Nix configuration
    This commit enables XDG portals for Wayland apps, including support for screen sharing and other functionalities. The configuration now includes the necessary packages, enhancing the user experience for applications that rely on these portals.

--- hosts/xps17-nixos/configuration.nix
+++ hosts/xps17-nixos/configuration.nix
@@ -387,6 +387,15 @@
     '';
   };
 
+  # XDG portals for Wayland apps (Deskflow, screen share, etc.)
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     wget
     xclip
