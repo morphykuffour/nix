1e7ff628f04756080c2321c80d758a6f41f8a2fc
Morphy Kuffour <morpkuff@gmail.com>
2026-02-07 20:52:36 -0500

    Disable morphEmacs in optiplex home.nix

--- hosts/optiplex-nixos/home.nix
+++ hosts/optiplex-nixos/home.nix
@@ -23,9 +23,9 @@
     home-manager = {
       enable = true;
     };
-    morphEmacs = {
-      enable = true;
-    };
+    # morphEmacs = { # Disabled: module not available on Linux
+    #   enable = true;
+    # };
     obs-studio = {
       enable = true;
       plugins = with pkgs.obs-studio-plugins; [
